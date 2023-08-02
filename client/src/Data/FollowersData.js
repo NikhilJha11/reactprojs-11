8e6902a78b2e10063ce6ec55bf18dc9754f9a542
unknown <nikhilarayn2021@gmail.com>
2023-08-02 15:59:11 +0530

    initial commit

---
+++ client/src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  { name: "Palki ", username: "palkinanda16", img: img1 },
+  { name: "varun ", username: "varunchopra12", img: img2 },
+  { name: "Anas", username: "md Anas", img: img3 },
+  { name: "shruti", username: "shruti14", img: img4 },
+];
---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "Naredra modi",
+      shares: 105,
+    },
+    {
+      name: "Osama bin laden",
+      shares: 60,
+    },
+    {
+      name: "Elon musk",
+      shares: 45.5,
+    },
+    {
+      name: "chandigarh university ",
+      shares: 42,
+    },
+    {
+      name: "India",
+      shares: 31.9,
+    },
+    {
+      name: "iphone",
+      shares: 20,
+    },
+  ];
+  
